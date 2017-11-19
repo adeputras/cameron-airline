e6b543d69d3cf4c9d15fb491864447bf0ce8600e
adeputras <poetrajerami@gmail.com>
2017-11-19 23:46:47 +0700

    check slider

--- js/script.js
+++ js/script.js
@@ -10,10 +10,38 @@
       });
     }
     mainTop();
+
+    //Slick Slider Main Slider
+    $('.main-slider').slick({
+      arrows: false,
+      pauseOnHover: false,
+      // autoplay: true,
+      speed: 1000,
+        fade: true,
+        cssEase: 'linear'
+    });
+
+    //Main Slider height
+    function sliderHeight() {
+      var winHeight = $(window).height();
+      var headerHeight = $('.section-header').outerHeight();
+      var contactInfo = $('.contact-info').outerHeight();
+      var sliderHeight = winHeight - contactInfo - headerHeight;
+
+      $('.main-slider .slick-track').css({
+        height: sliderHeight
+      });
+    }
+    sliderHeight();
+    
+    //Window Resize
     $(window).resize(function () {
       mainTop();
+      sliderHeight();
+  
     });
-    //menu mobile
+
+    //Menu Mobile
     if ('ontouchstart' in window) {
       var click = 'touchstart';
     } else {
@@ -76,6 +104,68 @@
       }, 100);
 
     }
+
+    if ($('.home').length > 0) {
+      //Polyfill to support video popup in internet explore
+      if (typeof Object.assign != 'function') {
+        Object.assign = function (target) {
+          'use strict';
+          if (target == null) {
+            throw new TypeError('Cannot convert undefined or null to object');
+          }
+
+          target = Object(target);
+          for (var index = 1; index < arguments.length; index++) {
+            var source = arguments[index];
+            if (source != null) {
+              for (var key in source) {
+                if (Object.prototype.hasOwnProperty.call(source, key)) {
+                  target[key] = source[key];
+                }
+              }
+            }
+          }
+          return target;
+        };
+      }
+
+      //Profile Video
+      $("#video-profile").videoPopup({
+        autoplay: 1,
+        controlsColor: 'white',
+        showVideoInformations: 0,
+        width: '100%',
+      });
+
+    }
+    
+    if ($('.home,.team').length > 0) {
+      //Section Award Parallax Image
+      // function parallax() {
+      //   var offset = $('.section-award').offset();
+      //   var imgPlx = $('.parallax-image').height() / 2 * 3;
+      //   var scrollPosition = $(window).scrollTop() - offset.top + imgPlx;
+
+      //   $('.parallax-image').css('top', (0 - (scrollPosition * .22)) + 'px')
+      // }
+      // $(window).bind('scroll', function (e) {
+      //   parallax();
+      // });
+    }
+
+    
+    //Slick Slider Community Slider
+    $('.community-slider').slick({
+      infinite: true,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      prevArrow: "<button type='button' class='slick-prev arrow-left'><i class='fa fa-angle-left'></i></button>",
+      nextArrow: "<button type='button' class='slick-next arrow-right'><i class='fa fa-angle-right'></i></button>"
+      //pauseOnHover: false,
+      //autoplay: true
+    });
+
+
   });
 
 })(jQuery);
